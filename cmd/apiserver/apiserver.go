dfde33e05db55b87964ae76f0c0ded6d7954ce0c
Adam Crosser <adam.crosser@praetorian.com>
2023-11-28 09:55:33 -0600

    Updated Middleware and Authorization Logic

--- cmd/apiserver/apiserver.go
+++ cmd/apiserver/apiserver.go
@@ -13,12 +13,49 @@ import (
 func main() {
 	r := chi.NewRouter()
 
+	// Assigns a unique request identiifer to each request which can help with debugging
 	r.Use(middleware.RequestID)
-	// r.Use(middleware.RealIP) // TODO: May be needed depending on if the API is deployed behind a proxy or other gateway
+
+	//
+	// TODO: Using the RealIP middleware could be dangerous if the application isn't deployed
+	//       behind a CDN or load balancer
+	//
+
+	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
+
 	r.Use(middleware.Recoverer)
 
 	r.Use(middleware.Timeout(60 * time.Second))
+
+	//
+	// Be extra fussy about the Content-Type header as a defense in-depth mechanism. Although, a
+	// lot of this only matters if the API is going to be used by browser-based cilents.
+	//
+	// https://portswigger.net/web-security/graphql/lab-graphql-csrf-via-graphql-api
+	// https://directdefense.com/csrf-in-the-age-of-json
+	//
+	// curl -H "Content-Type: application/x-www-form-urlencoded" -d "test=test "-X POST -v -H "Authorization: Bearer gae_ffd7bef1" http://localhost:8080/api/v1/private/test
+	//
+
+	r.Use(middleware.AllowContentType("application/json"))
+
+	//
+	// Be extra paranoid about caching of API responses by setting no cache
+	// headers in the responses for all API requests
+	//
+
+	r.Use(middleware.NoCache)
+
+	// NOTE: A list of available middlewares as part of the go-chi framework
+	// https://github.com/go-chi/chi/tree/master/middleware
+
+	// NOTE: A middleware that we could leverage for cross origin resource sharing
+	// https://github.com/go-chi/cors
+
+	// NOTE: This may be useful in terms of rate limiting for API endpoints
+	// https://github.com/go-chi/httprate
+	// https://github.com/go-chi/httprate-redis
 
 	r.Route("/api/v1/private/", func(r chi.Router) {
 
@@ -27,10 +64,31 @@ func main() {
 		//       hardcode secrets in source code like this for a production application
 		//
 
+		// The example gae_ prefix is used to make it easier to identify API tokens hardcoded
+		// in the source code of the application
+		//
+		// https://github.blog/2021-04-05-behind-githubs-new-authentication-token-formats/
+
 		apiKeys := map[string]string{
-			"ffd7bef1": "admin",
-			"98e8d5f4": "reguser",
+			"gae_ffd7bef1": "admin",
+			"gae_98e8d5f4": "reguser",
 		}
+
+		// TODO: It's probably better to pass a function to the authentication middleware
+		//       so that we can decouple the underlying datastore for tokens from the
+		//       implementation of the authorization middleware
+		//
+		// NOTE: Example below of how Go lets you define function as a first-class
+		//       data-type/value
+		// x := func(name string) string {
+		//	return "Hello " + name
+		// }
+		// x("Adam")
+
+		//
+		// Performs validation of API keys using the Authorization: Bearer $APITOKEN format
+		// curl -v -H "Authorization: Bearer gae_ffd7bef1" http://localhost:8080/api/v1/private/test
+		//
 
 		r.Use(authentication.Verifier(apiKeys))
 
@@ -48,6 +106,14 @@ func main() {
 			w.Write([]byte("{\"alive\": true}"))
 		})
 	})
+
+	//
+	// TODO: In scenarios where this API would be deployed behind a load balancer or other gateway
+	// my recommendation would be to leverage only HTTP/2 for communication with backend systems
+	// to reduce the risk of HTTP desync issues, etc. that occur with the ambiguity associated with
+	// HTTP/1.1 request parsing
+	// https://portswigger.net/research/http-desync-attacks-request-smuggling-reborn
+	//
 
 	err := http.ListenAndServe(":8080", r)
 	if err != nil {
--- pkg/authentication/authentication.go
+++ pkg/authentication/authentication.go
@@ -3,31 +3,39 @@ package authentication
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-func Verifier(apiKeys map[string]string) func(http.Handler) http.Handler {
+func Verifier(validAPIKeys map[string]string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		hfn := func(w http.ResponseWriter, r *http.Request) {
 			headers := r.Header
 
-			authHeaderVal := headers.Get("Authorization")
+			authHeaderString := headers.Get("Authorization")
 
-			if authHeaderVal == "" {
+			if authHeaderString == "" {
 				w.WriteHeader(http.StatusUnauthorized)
 				w.Write([]byte("Authorization header should include an API key"))
 				return
 			}
 
-			apiKey, found := apiKeys[authHeaderVal]
+			parts := strings.Split(authHeaderString, " ")
+			if len(parts) != 2 || parts[0] != "Bearer" {
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write([]byte("Invalid Authorization header format. It should be in the format: Bearer $APITOKEN"))
+				return
+			}
 
+			reqAPIKey := parts[1]
+			_, found := validAPIKeys[reqAPIKey]
 			if !found {
-				fmt.Printf("User attempted to authenticate with an invalid API key, key: %s\n", apiKey)
+				fmt.Printf("User attempted to authenticate with an invalid API key, key: %s\n", reqAPIKey)
 				w.WriteHeader(http.StatusUnauthorized)
 				w.Write([]byte("An invalid API key was specified in the Authorization header"))
 				return
 			}
 
-			fmt.Printf("User successfully authenticated with API key, key: %s\n", apiKey)
+			fmt.Printf("User successfully authenticated with API key, key: %s\n", reqAPIKey)
 			next.ServeHTTP(w, r)
 		}
 
